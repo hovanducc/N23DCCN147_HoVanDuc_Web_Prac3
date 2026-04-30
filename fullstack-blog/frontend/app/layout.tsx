032e7137bd3c2a9bdc9919d793e9f0760dfc5371
Ho Van Duc <ducho6699@gmail.com>
2026-04-30 14:50:48 +0700

    chore: khởi tạo project Next.js, Express và cấu hình cơ bản

---
+++ fullstack-blog/frontend/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Toaster } from 'react-hot-toast';
+import Providers from "./provider"; // Import file vừa tạo
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DevBlog - Lab 3",
+  description: "Fullstack Integration với Next.js và Express",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-full bg-[#0b0f1a]`}>
+        <Providers>
+          {children}
+        </Providers>
+        <Toaster position='top-right' />
+      </body>
+    </html>
+  );
+}
